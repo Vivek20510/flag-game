df8c400457c69dbc7169a98b27c8ae73cb21aa49
Vivek20510 <vivekbadodiya68@gmail.com>
2025-03-17 15:53:18 +0530

    secure

--- frontend/src/firebase/firebaseConfig.js
+++ frontend/src/firebase/firebaseConfig.js
@@ -1,19 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyDhiZ583YB38HXkmu8Bazjrfajq85xCIew",
-  authDomain: "guesstheflag-7e963.firebaseapp.com",
-  projectId: "guesstheflag-7e963",
-  storageBucket: "guesstheflag-7e963.firebasestorage.app",
-  messagingSenderId: "622050273178",
-  appId: "1:622050273178:web:ea93bc4dff91ce1e603c23",
-  measurementId: "G-PXEYNB6K2C"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
@@ -21,7 +17,5 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firestore
 const db = getFirestore(app);
-
-
 
 export { db };
